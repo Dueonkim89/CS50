79f8bcaadcbad87904e525a1aef6d57d1a01eec2
Dueon Kim <dueonkim89@yahoo.com>
2019-04-25 23:09:59 -0700

    styling and syntax

--- Week 2/crack.c
+++ Week 2/crack.c
@@ -24,7 +24,7 @@ int main(int argc, string argv[])
     // Create array of letters
     string possibleLettersArray[52] = {0};
 
-    for (int i = 0; i<PWcombos; i++)
+    for (int i = 0; i < PWcombos; i++)
     {
         // Cannot create a fixed char array, since it will always point to same location
         // in memory even when its in a loop!
@@ -52,22 +52,22 @@ int main(int argc, string argv[])
     // Create comboA, 2704
     comboA = malloc((PWcombos * 52) * sizeof(string));
 
-    for (int i = 0; i<PWcombos; i++)
+    for (int i = 0; i < PWcombos; i++)
     {
         // Add new permutations to combo A
-        for (int j = 0; j<PWcombos; j++)
+        for (int j = 0; j < PWcombos; j++)
         {
             char *new = malloc((2) * sizeof(string));
             strcpy(new, possibleLettersArray[i]);
             strcat(new, possibleLettersArray[j]);
-      			char *pw = crypt(new, salt);
-      			if (strcmp(pw, hashPW) == 0)
-      			{
-      				printf("%s\n", new);
-      				found = true;
-      				return 0;
-      			}
-			      // push this concat into old[counter]
+            char *pw = crypt(new, salt);
+            if (strcmp(pw, hashPW) == 0)
+            {
+                printf("%s\n", new);
+                found = true;
+                return 0;
+            }
+            // push this concat into old[counter]
             comboA[counter] = new;
             counter ++;
         }
@@ -81,21 +81,21 @@ int main(int argc, string argv[])
     // 140,608 combos, combo B
     comboB = malloc((PWcombos * 52) * sizeof(string));
 
-    for (int i = 0; i<PWcombos; i++)
+    for (int i = 0; i < PWcombos; i++)
     {
         // Add new permutations to array
-        for (int j = 0; j<52; j++)
+        for (int j = 0; j < 52; j++)
         {
             char *new = malloc((5) * sizeof(string));
             strcpy(new, comboA[i]);
             strcat(new, possibleLettersArray[j]);
-      			char *pw = crypt(new, salt);
-      			if (strcmp(pw, hashPW) == 0)
-      			{
-      				printf("%s\n", new);
-      				found = true;
-      				return 0;
-      			}
+            char *pw = crypt(new, salt);
+            if (strcmp(pw, hashPW) == 0)
+            {
+                printf("%s\n", new);
+                found = true;
+                return 0;
+            }
             comboB[counter] = new;
             counter ++;
         }
@@ -112,22 +112,22 @@ int main(int argc, string argv[])
     // Recreate comboA, 7,311,616 combos
     comboA = malloc((PWcombos * 52) * sizeof(string));
 
-    for (int i = 0; i<PWcombos; i++)
+    for (int i = 0; i < PWcombos; i++)
     {
         // Add new permutations to array
-        for (int j = 0; j<52; j++)
+        for (int j = 0; j < 52; j++)
         {
             char *new = malloc((5) * sizeof(string));
             strcpy(new, comboB[i]);
             strcat(new, possibleLettersArray[j]);
-	          char *pw = crypt(new, salt);
-      			// if match, set bool to true, return 0
-      			if (strcmp(pw, hashPW) == 0)
-      			{
-      				printf("%s\n", new);
-      				found = true;
-      				return 0;
-      			}
+            char *pw = crypt(new, salt);
+            // if match, set bool to true, return 0
+            if (strcmp(pw, hashPW) == 0)
+            {
+                printf("%s\n", new);
+                found = true;
+                return 0;
+            }
             comboA[counter] = new;
             counter ++;
         }
@@ -135,32 +135,32 @@ int main(int argc, string argv[])
         free(comboB[i]);
     }
 
-     // Free combo B
+    // Free combo B
     free(comboB);
     counter = 0;
     // 7,311,616
     PWcombos *= 52;
 
-    // 	Requesting too much memory from heap, this cannot be granted.
-    //	comboB = malloc((PWcombos * 52) * sizeof(5));
+    // Requesting too much memory from heap, this cannot be granted.
+    // comboB = malloc((PWcombos * 52) * sizeof(5));
 
-    for (int i = 0; i<PWcombos; i++)
+    for (int i = 0; i < PWcombos; i++)
     {
         // Find final permutations, 380,204,032 combos
-        for (int j = 0; j<52; j++)
+        for (int j = 0; j < 52; j++)
         {
             char *new = malloc((6) * sizeof(string));
             strcpy(new, comboA[i]);
             strcat(new, possibleLettersArray[j]);
 
-            //compare hash to input
+            // Compare hash to input
             char *pw = crypt(new, salt);
-      			if (strcmp(pw, hashPW) == 0)
-      			{
-      				printf("%s\n", new);
-      				found = true;
-      				return 0;
-      			}
+            if (strcmp(pw, hashPW) == 0)
+            {
+                printf("%s\n", new);
+                found = true;
+                return 0;
+            }
             //free memory immediately
             free(new);
         }
