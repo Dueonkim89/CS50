b474a40c398364f8917f6723c19431c0e3630366
Dueon Kim <dueonkim89@yahoo.com>
2019-05-13 23:03:09 -0700

    mario less in python

---
+++ Week 6/mario-less.py
@@ -0,0 +1,16 @@
+from cs50 import get_int
+
+height = get_int("Height: ")
+
+while height <= 0 or height > 8:
+    height = get_int("Height: ")
+
+for i in range(height):
+    for j in range(height):
+        if (j + i) + 1 < height:
+            print(" ", end="")
+        else:
+            print("#", end="")
+    print()
+
+    
